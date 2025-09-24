5b4ecf509e44dff0a53de2199d9c63abf4017685
Hydra777777 <speedydino777@outlook.com>
2025-09-24 11:53:18 +1000

    Initial Userscript setup

--- src/geofs-ai-pilot.user.js
+++ src/geofs-ai-pilot.user.js
@@ -1,1 +1,10 @@
-
+// ==UserScript==
+// @name        GeoFS AI Pilot
+// @namespace   http://tampermonkey.net/
+// @version     0.1
+// @description AI fly plane
+// @author      Hydra
+// @match       https://www.geo-fs.com/*
+// @grant       none
+// @run-at      document-idle
+// ==/UserScript==
